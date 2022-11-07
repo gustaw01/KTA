26b6130d8bcb9ac839f7f09c067cdc18422c12e1
Konrad <goostaw.projekt@gmail.com>
2022-11-07 15:31:19 +0100

    enemy moving script

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            Debug.Log(waypoint.name);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
+
